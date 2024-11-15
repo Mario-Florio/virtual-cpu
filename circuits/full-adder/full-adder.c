25118084d19409bfd82197472ae20c3fec6f1539
ketherball <121590283+ketherball@users.noreply.github.com>
2024-11-15 10:51:39 -0500

    feat: implement full adder functionality

--- circuits/full-adder/full-adder.c
+++ circuits/full-adder/full-adder.c
@@ -1,5 +1,16 @@
 #include "../../logic-gates/logic-gates.h"
 
 char* full_adder(char a, char b, char cin, char* output) {
+
+    char d = _xor(a, b);
+    char sum = _xor(d, cin);
+
+    char e = _and(a, b);
+    char f = _and(cin, d);
+    char cout = _xor(e, f);
+
+    output[0] = sum;
+    output[1] = cout;
+
     return output;
 }
