9a27f60fdfaa73f3f5990f417c8215b60bef7606
ketherball <121590283+ketherball@users.noreply.github.com>
2024-11-15 11:06:58 -0500

    feat: implement full adder subtractor structure

---
+++ circuits/full-adder-subtractor/full-adder-subtractor.c
@@ -0,0 +1,6 @@
+
+char* full_adder_subtractor(char a, char b, char cin,
+                            char sub, char* output) {
+
+    return output;
+}
---
+++ circuits/full-adder-subtractor/full-adder-subtractor.h
@@ -0,0 +1,7 @@
+#ifndef virtualcpu_circuits_fulladdersubtractor_h
+#define virtualcpu_circuits_fulladdersubtractor_h
+
+char* full_adder_subtractor(char a, char b, char cin,
+                            char sub, char* output);
+
+#endif
