97e43cc00ab63bc331805cedadfcdc28cc270ea5
ketherball <121590283+ketherball@users.noreply.github.com>
2024-11-15 10:22:34 -0500

    feat: implement logic gates structure

---
+++ logic-gates/logic-gates.c
@@ -0,0 +1,14 @@
+
+char _buffer(char on) {}
+
+char _not(char on) {}
+
+char _or(char a, char b) {}
+
+char _xor(char a, char b) {}
+
+char _nor(char a, char b) {}
+
+char _and(char a, char b) {}
+
+char _nand(char a, char b) {}
---
+++ logic-gates/logic-gates.h
@@ -0,0 +1,12 @@
+#ifndef virtualcpu_logicgates_h
+#define virtualcpu_logicgates_h
+
+char _buffer(char on);
+char _not(char on);
+char _and(char a, char b);
+char _nand(char a, char b);
+char _or(char a, char b);
+char _nor(char a, char b);
+char _xor(char a, char b);
+
+#endif
