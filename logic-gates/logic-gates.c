8e6e3eb4779894e61761df56a77df14f7da1ea64
ketherball <121590283+ketherball@users.noreply.github.com>
2024-11-15 10:26:30 -0500

    feat: implement logic gates functionality

--- logic-gates/logic-gates.c
+++ logic-gates/logic-gates.c
@@ -1,14 +1,28 @@
 
-char _buffer(char on) {}
+char _buffer(char on) {
+    return on;
+}
 
-char _not(char on) {}
+char _not(char on) {
+    return !on;
+}
 
-char _or(char a, char b) {}
+char _or(char a, char b) {
+    return a || b;
+}
 
-char _xor(char a, char b) {}
+char _xor(char a, char b) {
+    return a ^ b;
+}
 
-char _nor(char a, char b) {}
+char _nor(char a, char b) {
+    return !(a || b);
+}
 
-char _and(char a, char b) {}
+char _and(char a, char b) {
+    return a && b;
+}
 
-char _nand(char a, char b) {}
+char _nand(char a, char b) {
+    return !(a && b);
+}
