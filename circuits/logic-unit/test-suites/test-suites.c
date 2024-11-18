9871868678393d162e8c2adeb0aa93719c6a069d
ketherball <121590283+ketherball@users.noreply.github.com>
2024-11-18 14:56:22 -0500

    test: update mode inputs for each test

--- circuits/logic-unit/test-suites/test-suites.c
+++ circuits/logic-unit/test-suites/test-suites.c
@@ -7,14 +7,6 @@ void logicunit_testsuites() {
     suite("========LOGIC UNIT========", truthtable_testsuite);
 }
 
-// M1 M2 M3 -> Logic Gate
-// 0  0  0  -> AND
-// 0  0  1  -> NAND
-// 0  1  0  -> OR
-// 1  0  0  -> NOR
-// 1  1  0  -> XOR
-// 1  1  1  -> XNOR
-
 static void truthtable_testsuite() {
     it("A B M0 M1 M2 -> Conclusion", 1);
 
@@ -23,37 +15,88 @@ static void truthtable_testsuite() {
 
     it("AND", 1);
     it("0 0 0  0  0  ->  0", logic_unit(a, b, m0, m1, m2) == 0);
+
+    a = 1, b = 0;
     it("1 0 0  0  0  ->  0", logic_unit(a, b, m0, m1, m2) == 0);
+
+    a = 0, b = 1;
     it("0 1 0  0  0  ->  0", logic_unit(a, b, m0, m1, m2) == 0);
+
+    a = 1, b = 1;
     it("1 1 0  0  0  ->  1", logic_unit(a, b, m0, m1, m2) == 1);
 
     it("NAND", 1);
+    m0 = 0, m1 = 0, m2 = 1;
+
+    a = 0, b = 0;
     it("0 0 0  0  1  ->  1", logic_unit(a, b, m0, m1, m2) == 1);
+
+    a = 1, b = 0;
     it("1 0 0  0  1  ->  1", logic_unit(a, b, m0, m1, m2) == 1);
+
+    a = 0, b = 1;
     it("0 1 0  0  1  ->  1", logic_unit(a, b, m0, m1, m2) == 1);
+
+    a = 1, b = 1;
     it("1 1 0  0  1  ->  0", logic_unit(a, b, m0, m1, m2) == 0);
 
     it("OR", 1);
+    m0 = 0, m1 = 1, m2 = 0;
+
+    a = 0, b = 0;
     it("0 0 0  1  0  ->  0", logic_unit(a, b, m0, m1, m2) == 0);
+
+    a = 1, b = 0;
     it("1 0 0  1  0  ->  1", logic_unit(a, b, m0, m1, m2) == 1);
+
+    a = 0, b = 1;
     it("0 1 0  1  0  ->  1", logic_unit(a, b, m0, m1, m2) == 1);
+
+    a = 1, b = 1;
     it("1 1 0  1  0  ->  1", logic_unit(a, b, m0, m1, m2) == 1);
     
     it("NOR", 1);
+    m0 = 1, m1 = 0, m2 = 0;
+
+    a = 0, b = 0;
     it("0 0 1  0  0  ->  1", logic_unit(a, b, m0, m1, m2) == 1);
+
+    a = 1, b = 0;
     it("1 0 1  0  0  ->  0", logic_unit(a, b, m0, m1, m2) == 0);
+
+    a = 0, b = 1;
     it("0 1 1  0  0  ->  0", logic_unit(a, b, m0, m1, m2) == 0);
+
+    a = 1, b = 1;
     it("1 1 1  0  0  ->  0", logic_unit(a, b, m0, m1, m2) == 0);
 
     it("XOR", 1);
+    m0 = 1, m1 = 1, m2 = 0;
+
+    a = 0, b = 0;
     it("0 0 1  1  0  ->  0", logic_unit(a, b, m0, m1, m2) == 0);
+
+    a = 1, b = 0;
     it("1 0 1  1  0  ->  1", logic_unit(a, b, m0, m1, m2) == 1);
+
+    a = 0, b = 1;
     it("0 1 1  1  0  ->  1", logic_unit(a, b, m0, m1, m2) == 1);
+
+    a = 1, b = 1;
     it("1 1 1  1  0  ->  0", logic_unit(a, b, m0, m1, m2) == 0);
 
     it("XNOR", 1);
-    it("0 0 1  1  0  ->  1", logic_unit(a, b, m0, m1, m2) == 1);
-    it("1 0 1  1  0  ->  0", logic_unit(a, b, m0, m1, m2) == 0);
-    it("0 1 1  1  0  ->  0", logic_unit(a, b, m0, m1, m2) == 0);
-    it("1 1 1  1  0  ->  1", logic_unit(a, b, m0, m1, m2) == 1);
+    m0 = 1, m1 = 1, m2 = 1;
+
+    a = 0, b = 0;
+    it("0 0 1  1  1  ->  1", logic_unit(a, b, m0, m1, m2) == 1);
+
+    a = 1, b = 0;
+    it("1 0 1  1  1  ->  0", logic_unit(a, b, m0, m1, m2) == 0);
+
+    a = 0, b = 1;
+    it("0 1 1  1  1  ->  0", logic_unit(a, b, m0, m1, m2) == 0);
+
+    a = 1, b = 1;
+    it("1 1 1  1  1  ->  1", logic_unit(a, b, m0, m1, m2) == 1);
 }
