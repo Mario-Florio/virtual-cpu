5411c4de98a00dc9c0c2232f9b8826ad162f6315
ketherball <121590283+ketherball@users.noreply.github.com>
2024-11-18 13:45:06 -0500

    feat: implement logic unit structure

---
+++ circuits/logic-unit/logic-unit.c
@@ -0,0 +1,7 @@
+#include "logic-unit.h"
+#include "../../logic-gates/logic-gates.h"
+
+Bit logic_unit(Bit a, Bit b, Bit m0, Bit m1, Bit m3) {
+    Bit conclusion = 0;
+    return conclusion;
+}
---
+++ circuits/logic-unit/logic-unit.h
@@ -0,0 +1,8 @@
+#ifndef virtualcpu_circuits_logicunit_h
+#define virtualcpu_circuits_logicunit_h
+
+#include "../../common.h"
+
+Bit logic_unit(Bit a, Bit b, Bit m0, Bit m1, Bit m3);
+
+#endif
